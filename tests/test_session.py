69a7dff960cf32d8ac1323c12d1c9385469df7c2
Cor Zuurmond <jczuurmond@protonmail.com>
2022-01-28 10:46:36 +0100

    Add test for cursor

---
+++ tests/test_session.py
@@ -0,0 +1,13 @@
+from pyspark.sql import SparkSession
+
+from pytest_dbt_core.session import Cursor
+
+
+def test_cursor_execute_fetchall_returns_one_row(spark_session: SparkSession) -> None:
+    """Test cursor if fetchall cursor returns one row."""
+
+    cursor = Cursor()
+    cursor.execute("SELECT 1")
+    rows = cursor.fetchall()
+
+    assert len(rows) == 1
